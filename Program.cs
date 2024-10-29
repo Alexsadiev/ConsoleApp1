2590983b36f63767de0baf480b752343e44d1900
Qnix <Qnix@DESKTOP-SM1MA27>
2024-10-29 20:13:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x >= 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
